15fc19e81ad6dd2162cc52e550c113c87525e6e0
anac-mendes <mendes.anac2@gmail.com>
2026-01-11 12:25:30 -0800

    fix mobile menu navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { HashLink } from "react-router-hash-link";
 
 import { useInfos } from "../contexts/InfoProvider";
@@ -10,10 +10,17 @@ const Header = ({ children }) => {
   const aboutRef = useRef(null);
   const portfolioRef = useRef(null);
   const contactRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const refNav = useRef(null);
 
-  // const { hash } = useLocation();
+  const handleToogleMobMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
   useEffect(() => {
     const hash = Object.keys(sizes).find((key) => sizes[key] > 0);
@@ -38,43 +45,56 @@ const Header = ({ children }) => {
   useEffect(() => {
     const hash = Object.keys(sizes).find((key) => sizes[key] > 0);
     if (hash === "home") {
-      refNav.current.classList.remove("noHeaderBackground");
+      refNav.current?.classList.remove("noHeaderBackground");
     } else {
-      refNav.current.classList.add("noHeaderBackground");
+      refNav.current?.classList.add("noHeaderBackground");
     }
   }, [sizes]);
+
+  useEffect(() => {
+    if (refNav.current) {
+      refNav.current.classList.toggle("open", menuOpen);
+    }
+  }, [menuOpen]);
 
   return (
     <>
       <nav id="nav-wrap">
-        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
-          Show navigation
-        </a>
-        <a className="mobile-btn" href="#home" title="Hide navigation">
-          Hide navigation
-        </a>
+        <div
+          className="mobile-buttom-div"
+          role="button"
+          tabIndex={0}
+          aria-label="Toggle list"
+          onClick={handleToogleMobMenu}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              e.preventDefault();
+              handleToogleMobMenu();
+            }
+          }}
+        />
 
         <ul id="nav" className="nav" ref={refNav}>
           <li className="current" ref={homeRef} id="#homeRef">
-            <HashLink smooth to="/#home">
+            <HashLink smooth to="/#home" onClick={closeMenu}>
               Home
             </HashLink>
           </li>
 
           <li ref={aboutRef} id="#aboutRef">
-            <HashLink smooth to="/#about">
+            <HashLink smooth to="/#about" onClick={closeMenu}>
               About
             </HashLink>
           </li>
 
           <li ref={portfolioRef} id="#portfolioRef">
-            <HashLink smooth to="/#portfolio">
+            <HashLink smooth to="/#portfolio" onClick={closeMenu}>
               Portfolio
             </HashLink>
           </li>
 
           <li ref={contactRef} id="#contactRef">
-            <HashLink smooth to="/#contact">
+            <HashLink smooth to="/#contact" onClick={closeMenu}>
               Contact
             </HashLink>
           </li>
